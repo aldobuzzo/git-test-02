f903e82e114f7304ca1724e8f552d07788d9b280
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-03-01 10:10:58 +0100

    Test Merge Manually OK
    Test Merge Manually OK

--- test_eclipse/workspace/TestGit01/src/test/git/src/SayGoodNight.java
+++ test_eclipse/workspace/TestGit01/src/test/git/src/SayGoodNight.java
@@ -16,4 +16,9 @@ public class SayGoodNight {
 		System.out.println("Good Night Dad!");
 	}
 	//hotfix 20190226-01 fine
+	//hotfix 20190228-01 inizio
+	public void sayGoodNightAgain(){
+		System.out.println("Good Night Again!");
+	}
+	//hotfix 20190228-01 fine
 }

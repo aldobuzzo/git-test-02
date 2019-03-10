501da11ee1bf721b8f65a861b321126281fcbc74
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-03-10 13:17:09 +0100

    Feature Having Lunch

---
+++ test_eclipse/workspace/TestGit01/src/test/git/src/SayHavingLunch.java
@@ -0,0 +1,11 @@
+package test.git.src;
+
+public class SayHavingLunch {
+
+	public SayHavingLunch() {
+		super();		
+	}
+	public void sayHavingLunch(){
+		System.out.println("I am having lunch now!");
+	}
+}

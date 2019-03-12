243254987f89cd2a16ce8976bed098bae472740a
aldobuzzo <aldo_buzzo@it.ibm.com>
2019-03-12 09:46:40 +0100

    Commit Having Dinner With Mother

---
+++ test_eclipse/workspace/TestGit01/src/test/git/src/HavingDinnerWithMother.java
@@ -0,0 +1,11 @@
+package test.git.src;
+
+public class HavingDinnerWithMother {
+
+	public HavingDinnerWithMother() {
+		super();
+	}
+	public void havingDinnerWithMother() {
+		System.out.println("I am having dinner with Mum!");
+	}
+}
